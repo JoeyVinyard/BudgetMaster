73dd9c5134ce6a75a5723cf908a72f80b1f749f4
chris-coraggio <ccoraggi@purdue.edu>
2017-01-21 09:39:41 -0500

    migrate capital one functions to inde.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var app = require('express')();
-let request = require("request");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-
-let baseUrl = "http://api.reimaginebanking.com/";
-let keyUrl = "?key=335c078a708beb9fffbe11ee6a51364e";
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -54,6 +50,18 @@ io.on('connection', function(socket){
 	});
 });
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+//--------------------------------------
+//--------Capital One Functions---------
+//--------------------------------------
+
+let request = require("request");
+let baseUrl = "http://api.reimaginebanking.com/";
+let keyUrl = "?key=335c078a708beb9fffbe11ee6a51364e";
+
 function createCustomer(firstName, lastName, streetNum, streetName, city, state, zip){
 	if(firstName === undefined){
 		firstName = "Katy";
@@ -76,22 +84,10 @@ function createCustomer(firstName, lastName, streetNum, streetName, city, state,
 	if(zip === undefined){
 		zip = "12345";
 	}
-	console.log(JSON.stringify(
-    		{
-			  "first_name": firstName,
-			  "last_name": lastName,
-			  "address": {
-			    "street_number": streetNum,
-			    "street_name": streetName,
-			    "city": city,
-			    "state": state,
-			    "zip": zip
-			  }
-		}))
     request.post({
     	url: baseUrl + "customers" + keyUrl,
-    	json: (
-    		{
+    	json:
+			{
 			  "first_name": firstName,
 			  "last_name": lastName,
 			  "address": {
@@ -101,7 +97,7 @@ function createCustomer(firstName, lastName, streetNum, streetName, city, state,
 			    "state": state,
 			    "zip": zip
 			  }
-		})
+			}
 	}, function(error, response, body){
 		console.log("hello",body.objectCreated._id); //customer id
 		//plug this into the database
@@ -118,12 +114,84 @@ function createCustomer(firstName, lastName, streetNum, streetName, city, state,
 			fs.close(fd,function(err){console.log(err);});
 		});
 	});
+}
+
+function createAccount(customerID, accountType, accountNickname, rewards, balance){
+	if (accountType === undefined) {
+		accountType = "Credit Card";
+	}
+	if(accountNickname === undefined){
+		accountNickname = "Account";
+	}
+	if(rewards === undefined){
+		rewards = 0;
+	}
+	if(balance === undefined){
+		balance = 0;
+	}
+	request.post({
+        url: baseUrl + "customers/" + customerID + "/accounts" + keyUrl,
+        json:
+        	{
+				"type": accountType,
+				"nickname": accountNickname,
+				"rewards": rewards,
+				"balance": balance,
+				"account_number": generateRandomNumber(16)
+			})
+	},function(error, response, body){
+		console.log(response["_id"]); //account id
+		//plug this into the database
 	});
 }
 
+function generateRandomNumber(length){
+	var num = "";
+	for(int i=0; i < length; i++){
+		num += Math.floor(Math.random() * 10).toString();
+	}
+	return num;
+}
 
+function makePurchase(accountID, merchantID, medium, purchaseDate, amount, description){
+	if(merchantID === undefined){
+		merchantID = generateRandomNumber(24);
+	}
+	if(medium === undefined){
+		medium = "balance";
+	}
+	if(purchaseDate === undefined){
+		purchaseDate="2017-01-01";
+	}
+	if(amount === undefined){
+		amount = 0.01;
+	}
+	if(description === undefined){
+		description = "Description";
+	}
+	request.post({
+        url: baseUrl + "accounts/" + accountID + "/purchases" + keyUrl,
+        json:
+        	{
+				  "merchant_id": merchantID,
+				  "medium": medium,
+				  "purchase_date": purchaseDate,
+				  "amount": amount,
+				  "description": description
+			}
+	},function(error, response, body){
+		console.log(response["_id"]); //purchase id
+		//not sure if this needs to go into the database
+	});
+}
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
+function getPurchases(customerID){
+	request(baseUrl + "accounts/" + customerID + "/purchases" + keyUrl,
+		function(error, response, body){
+			console.log(response["_id"]); //purchase ID
+			console.log(response["purchase_date"]);
+			console.log(response["amount"]);
+			console.log(response["description"]);
+			//maybe just grab the whole object
+		});
+}
