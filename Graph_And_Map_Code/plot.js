0ca93ada88e97ff36d7fb6306e0c1804ad90dfe9
Calvin Henry <calvinhenry60@gmail.com>
2017-01-21 14:04:51 -0500

    Made graphs follow correct format

--- Graph_And_Map_Code/plot.js
+++ Graph_And_Map_Code/plot.js
@@ -1,4 +1,4 @@
-var TESTER = document.getElementById('tester');
+ var TESTER = document.getElementById('tester');
 var x;
 var plotData = new Array(4);
 for(x = 0; x < plotData.length; x ++){
@@ -11,6 +11,14 @@ function plotLineGraph(data){
     
 }
 function plotHeatMap(data){
+
+    data.foreach(function(p){
+	var timeStamp = p.purchase_date;
+	var time = timeStamp.split('-');
+	plotData[Math.floor(time[2]/6)][time[1] = p.amount_spent;
+    }
+
+/*
     console.log(data);
     var week = Math.floor(date.getDay() / 7) + 1;
     var month = date.getMonth();
@@ -24,19 +32,33 @@ function plotHeatMap(data){
 		month = 12;
 	    }
 	}
-    }
+    }*/
     var graph = [
       {
 	  z: plotData,
 	  x: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septermpber', 'October', 'November', 'December'],
 	  y: ['Week One', 'Week Two', 'Week Three', 'Week Four'],
-	  type: 'heatmap'
+	  colorscale: [
+	      ['0.0', 'rgb(240,240,240)'],
+	      ['0.111111111111', 'rgb(150,255,150)'],
+	      ['0.222222222222', 'rgb(0,255,0)'],
+	      ['0.333333333333', 'rgb(140,255,0)'],
+	      ['0.444444444444', 'rgb(200,255,0)'],
+	      ['0.555555555556', 'rgb(255,255,0)'],
+	      ['0.666666666667', 'rgb(255,200,0)'],
+	      ['0.777777777778', 'rgb(255,150,0)'],
+	      ['0.888888888889', 'rgb(255,75,0)'],
+	      ['1.0', 'rgb(255,0,0)']
+	        ],
+	  type: 'heatmap',
+	  xgap: 5,
+	  ygap: 5
       }
     ];
     Plotly.plot(TESTER,graph);
 }
 
-plot([1,.4,.6,.2,.9,0]);
+plotHeatMap([1,.4,.6,.2,.9,0]);
 
 /* Current Plotly.js version */
 console.log( Plotly.BUILD );
