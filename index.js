52f56a963735fd996c4501b469934345558914b1
JoeyVinyard <jviny98@yahoo.com>
2017-01-21 16:35:54 -0500

    Work on info

--- index.js
+++ index.js
@@ -142,12 +142,8 @@ io.on('connection', function(socket){
 							amount_spent: data[p].amount,
 							purchase_date: data[p].purchase_date,
 							desc: data[p].description
-						}
-						var forCalvin = {
 							lat: body.geocode.lat,
 							lng: body.geocode.lng,
-							category: body.category[0],
-							merchant_name: body.name
 						}
 						//Send andrew info
 					});
@@ -164,89 +160,6 @@ http.listen(3000, function(){
 //--------------------------------------
 //--------Capital One Functions---------
 //--------------------------------------
-function createCustomer(firstName, lastName, streetNum, streetName, city, state, zip){
-	if(firstName === undefined){
-		firstName = "Katy";
-	}
-	if(lastName === undefined){
-		lastName = "Voor";
-	}
-	if(streetNum === undefined){
-		streetNum = "1234";
-	}
-	if(streetName === undefined){
-		streetName = "Elm Street";
-	}
-	if(city === undefined){
-		city = "West Lafayette";
-	}
-	if(state === undefined){
-		state = "IN";
-	}
-	if(zip === undefined){
-		zip = "12345";
-	}
-    request.post({
-    	url: baseUrl + "customers" + keyUrl,
-    	json:
-			{
-			  "first_name": firstName,
-			  "last_name": lastName,
-			  "address": {
-			    "street_number": streetNum,
-			    "street_name": streetName,
-			    "city": city,
-			    "state": state,
-			    "zip": zip
-			  }
-			}
-	}, function(error, response, body){
-		createAccount(body.objectCreated._id);
-		console.log("hello",body.objectCreated._id); //customer id
-		//plug this into the database
-		fs.open('db.txt', 'a', function(err, fd) {
-			if (err) {
-				return console.error(err);
-			}
-			fs.appendFile(fd,"\n"+"user.name"+":"+"user.pass"+":"+body.objectCreated._id,function(err){
-				if(err){
-					socket.emit('regres',"Fail");
-					console.log(err);
-				}
-				//socket.emit('regres',"Success!");
-				fs.close(fd,function(err){console.log(err);});
-			});
-		});
-	});
-}
-
-function createAccount(customerID, accountType, accountNickname, rewards, balance){
-	if (accountType === undefined) {
-		accountType = "Credit Card";
-	}
-	if(accountNickname === undefined){
-		accountNickname = "Account";
-	}
-	if(rewards === undefined){
-		rewards = 0;
-	}
-	if(balance === undefined){
-		balance = 0;
-	}
-	request.post({
-        url: baseUrl + "customers/" + customerID + "/accounts" + keyUrl,
-        json:
-        	{
-				"type": accountType,
-				"nickname": accountNickname,
-				"rewards": rewards,
-				"balance": balance,
-				"account_number": generateRandomNumber(16)
-			}
-	},function(error, response, body){
-	    makeRandomPurchases(body.objectCreated._id, 30);
-	});
-}
 
 function generateRandomNumber(length){
 	var num = "";
