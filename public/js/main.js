9cdb2554431e643424e7d8217f175ec6475a09db
Andrew Bass <andybass009@gmail.com>
2017-01-21 22:41:43 -0500

    Cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -95,33 +95,22 @@ $(document).ready(function() {
     socket.emit("loadData", { custId: localStorage.customerId });
     socket.on("receiveData", function(data) {
         sortDates(data);
-        plotHeadMap(data);
     });
     
     socket.on("create-map", function(loc) {
         createMap(loc);
     });
 
+    socket.on("endData", function() {
+
+    });
+
 //     socket.on("add-marker", function(marker) {
 //         addMarker(marker.location, marker.name, marker.price);
 //     });
 	initializePlotlyElements();
-
-	var forAndrew = [{
-					amount_spent: "1.02",
-					purchase_date: "2016-12-12",
-				},
-				{
-					amount_spent: "102",
-					purchase_date: "2016-12-16",
-				},
-				{
-					amount_spent: "13.85",
-					purchase_date: "2017-01-12",
-				},
-				];
-	plotHeatMap(forAndrew);
 });
+
 var purchaseList = $("<div>").addClass("purchase-list");
 function createPurchase(name, date, amountDollars) {
     var purchase = $("<div>").addClass("purchase");
@@ -236,63 +225,8 @@ function initializePlotlyElements(){
 	// AVGWEEK = $("avgweek").get(0);
 }
 
-
-var x;
-var plotData = new Array(4);
-for(x = 0; x < plotData.length; x++){
-    plotData[x] = new Array(12);
-}
-var date = new Date();
-
-function plotLineGraph(data){
-    
-    
-}
-function plotBarGraph(data){
-    var spending = data.map(function(o){
-	return o.amount_spent;
-    });
-    var present_day = date.getDay();
-    var sum = 0;
-    var max = 0;
-    var min = 10000000;
-    var lastWeeklySum = 0;
-    var weeklySum = 0;
-    for(var i = 0; i < data.length; i ++){
-	var transaction_day = purchase_date.substring(lastIndexOf("-"));
-	if(present_day - transaction_day < 7){
-	    weeklySum += data.amount_spent;
-	}else if(present_day - transaction_day < 14){
-	    lastWeeklySum  += data.amount_spent;
-	}
-	sum += data.amount_spent;
-    }
-    var total_weekly_average = sum/12;
-    
-    var data = [{
-	type: 'bar',
-	x: [weeklySum, lastWeeklySum, total_weekly_average],
-	y: ['This Weeks Average', 'Last Week Average', 'Average of The Weekly Averages'],
-	orientation: 'h'
-    }];
-
-
-    Plotly.newPlot(BARGRAPH, data);
-    
-    var scale = [0];
-    for(var i = 0; i < 10; i ++){
-	
-    }	    
-}
-
 function plotHeatMap(data){
 
-    console.log(data);
-    data.forEach(function(p){
-        var timeStamp = p.purchase_date;
-        var time = timeStamp.split('-');
-        plotData[Math.floor(time[2]/7)][time[1]-1] = p.amount_spent;
-    });
 }
 
 //console.log( Plotly.BUILD );
