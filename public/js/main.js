bcdc6f7c57f04a307691a614c40484fa6edb7b6f
Calvin Henry <calvinhenry60@gmail.com>
2017-01-21 20:24:53 -0500

    Please don't kill me github

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-//----------------
+;//----------------
 //Google Maps Junk
 //----------------
 
@@ -133,14 +133,50 @@ function plotLineGraph(data){
     
     
 }
+function plotBarGraph(data){
+    var spending = data.map(function(o){
+	return o.amount_spent;
+    });
+    var present_day = date.getDay();
+    var sum = 0;
+    var max = 0;
+    var min = 10000000;
+    var lastWeeklySum = 0;
+    var weeklySum = 0;
+    for(var i = 0; i < data.length; i ++){
+	var transaction_day = purchase_date.substring(lastIndexOf("-"));
+	if(present_day - transaction_day < 7){
+	    weeklySum += data.amount_spent;
+	}else if(present_day - transaction_day < 14){
+	    lastWeeklySum  += data.amount_spent;
+	}
+	sum += data.amount_spent;
+    }
+    var total_weekly_average = sum/12;
+    
+    var data = [{
+	type: 'bar',
+	x: [weeklySum, lastWeeklySum, total_weekly_average],
+	y: ['This Weeks Average', 'Last Week Average', 'Average of The Weekly Averages'],
+	orientation: 'h'
+    }];
 
+    Plotly.newPlot('myDiv', data);
+    
+    var scale = [0];
+    for(var i = 0; i < 10; i ++){
+	
+    }
+			    
+			    
+}
 function plotHeatMap(data){
 
     data.foreach(function(p){
 	var timeStamp = p.purchase_date;
 	var time = timeStamp.split('-');
-	plotData[Math.floor(time[2]/6)][time[1] = p.amount_spent;
-    }
+	plotData[Math.floor(time[2]/6)][time[1]] = p.amount_spent;
+    });
 
     var graph = [
       {
